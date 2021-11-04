aacc37abe5a37fb100015ebb65725de240012693
Jinny <noh5013@naver.com>
2021-11-04 15:17:50 +0900

    test

--- app/src/main/java/com/fourth/ondaeng/MainActivity.java
+++ app/src/main/java/com/fourth/ondaeng/MainActivity.java
@@ -16,5 +16,5 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
     }
-
+ KimYuDi is BaBo and MeongChungEE
 }
