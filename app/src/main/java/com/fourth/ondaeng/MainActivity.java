9c4a4a0271a6d4209d6453f3c8517ee6a2b7f190
sdc <windman966@naver.com>
2021-11-04 14:38:54 +0900

    base

---
+++ app/src/main/java/com/fourth/ondaeng/MainActivity.java
@@ -0,0 +1,20 @@
+package com.fourth.ondaeng;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fourth.ondaeng.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+}
