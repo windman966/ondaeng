5ee855a4eb3ecfece4d8fafc2429df00dcc3bb3e
Jinny <noh5013@naver.com>
2021-11-05 17:39:59 +0900

    splash

--- app/src/main/java/com/fourth/ondaeng/CommunityActivity.java
+++ app/src/main/java/com/fourth/ondaeng/CommunityActivity.java
@@ -1,22 +1,14 @@
 package com.fourth.ondaeng;
 
-import android.os.Bundle;
-import android.os.PersistableBundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.fourth.ondaeng.databinding.ActivityCommunityBinding;
-import com.fourth.ondaeng.databinding.ActivityMainBinding;
+import android.os.Bundle;
 
 public class CommunityActivity extends AppCompatActivity {
 
-    private ActivityCommunityBinding binding;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityCommunityBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
+        setContentView(R.layout.activity_community);
     }
 }
