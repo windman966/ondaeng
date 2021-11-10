a4485a68a43e9b4e5850c503491026dcb81c4df7
sdc <windman966@naver.com>
2021-11-10 17:41:20 +0900

    1110dayEnd by sdc

--- app/src/main/java/com/fourth/ondaeng/CareActivity.java
+++ app/src/main/java/com/fourth/ondaeng/CareActivity.java
@@ -4,16 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.fourth.ondaeng.databinding.ActivityMainBinding;
+import com.fourth.ondaeng.databinding.ActivityCareBinding;
 
 public class CareActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private ActivityCareBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityCareBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
     }
 }
