8470e6f7a8b2d53fc245a515a58d8ba1e18a6328
Jinny <noh5013@naver.com>
2021-11-11 15:44:08 +0900

    커뮤니티

--- app/src/main/java/com/fourth/ondaeng/CommunityWriting.java
+++ app/src/main/java/com/fourth/ondaeng/CommunityWriting.java
@@ -11,14 +11,14 @@ public class CommunityWriting extends AppCompatActivity {
     EditText edit_title;
     EditText edit_content;
     Button bpost;
-    String user_id = "";
+    String user_id = ""; //로그인에서 유저 id 가져오기
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community_writing);
 
-        //listActivity 가 무슨 activity인지 체크 -> 코드 추가해주기
+        //listActivity 가 무슨 activity인지 체크
         user_id = getIntent().getStringExtra("user_id");
 
         //컴포넌트 초기화
