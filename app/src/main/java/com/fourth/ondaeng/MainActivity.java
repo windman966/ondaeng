ce3308bda684bbfbec8515ba3754903e293da7d2
cyh <dusgh096@naver.com>
2021-11-12 10:57:40 +0900

    cyh

--- app/src/main/java/com/fourth/ondaeng/MainActivity.java
+++ app/src/main/java/com/fourth/ondaeng/MainActivity.java
@@ -1,7 +1,5 @@
 package com.fourth.ondaeng;
 
-
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
@@ -14,36 +12,41 @@ import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.fourth.ondaeng.databinding.ActivityDrawerBinding;
 import com.fourth.ondaeng.databinding.ActivityMainBinding;
 
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 import me.relex.circleindicator.CircleIndicator3;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private ActivityDrawerBinding activityDrawerBinding;
     private DrawerLayout drawerLayout;
     private View drawerView;
     private ViewPager2 viewPager2;
     private CircleIndicator3 mIndicator;
 
-    //커뮤니티 어댑터
-    private ListView listView;
-    private CommunityAdapter communityAdapter;
-    private ArrayList<community_listitems> community_listitems;
-    private Object list_community_items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        activityDrawerBinding = ActivityDrawerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+//        아이디 값 받아오기
+        String id = (String) appData.id;
+        TextView nnOnNav = findViewById(R.id.nickNameOnNav);
+        nnOnNav.setText(id);
+
+//        Toast.makeText(getApplicationContext(),id+" appData에서 받음",Toast.LENGTH_SHORT).show();
 
         //강아지등록증 페이저
         viewPager2 = findViewById(R.id.dogIdCardPager);
@@ -51,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         //네비게이션 메뉴 코드
         drawerLayout = binding.drawerLayout;
         drawerView = (View)findViewById(R.id.drawer);
+
         Button btn_open = (Button)findViewById(R.id.btn_open);
         btn_open.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 return true;
             }
+
         });
 
         //등록증 데이터 및 코드 공유유
@@ -89,8 +94,8 @@ public class MainActivity extends AppCompatActivity {
         Intent careIntent = new Intent(this, CareActivity.class);
         Intent commIntent = new Intent(this, CommunityActivity.class);
         Intent walkIntent = new Intent(this, WalkActivity.class);
-        Intent hospIntent = new Intent(this, HospitalActivity.class);
         //마이페이지 이동
+
         findViewById(R.id.goToMyPage).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -124,25 +129,6 @@ public class MainActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.horizon_enter,R.anim.none);
             }
         });
-
-
-        //커뮤니티 리스트뷰, 어댑터
-        listView = (ListView)findViewById(R.id.community_listView);
-        list_community_items = new ArrayList<community_listitems>();
-
-        //커뮤니티 게시물(인앱)
-        // userid, title, content, category, date
-        community_listitems.add(new community_listitems("닉네임1", "간식 추천", "강아지 간식 추천해주세요", "양육 꿀팁", new Date(System.currentTimeMillis())));
-        community_listitems.add(new community_listitems("닉네임2", "장난감 나눔해요", "강아지 장난감 무료나눔합니다", "나눔", new Date(System.currentTimeMillis())));
-        community_listitems.add(new community_listitems("닉네임3", "샵 추천", "학동역 미용 잘 하는 곳 추천해주세요", "내 동네", new Date(System.currentTimeMillis())));
-        community_listitems.add(new community_listitems("닉네임4", "배변 훈련방법", "강아지 배변 훈련하는 방법 알려드려요!", "양육 꿀팁", new Date(System.currentTimeMillis())));
-        community_listitems.add(new community_listitems("닉네임5", "간식 나눔", "강아지 간식 나눔합니다", "나눔", new Date(System.currentTimeMillis())));
-
-
-        //어댑터 연결, 객체생성
-        communityAdapter = new CommunityAdapter(getApplicationContext(), community_listitems);
-        //communityAdapter = new CommunityAdapter(MainActivity.this, (ArrayList<com.fourth.ondaeng.community_listitems>) list_community_items);
-        listView.setAdapter(communityAdapter);
 
 
     }
