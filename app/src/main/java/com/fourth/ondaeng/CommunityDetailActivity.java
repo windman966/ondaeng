de54281cca990749010a856152006f65954e84f0
cyh <dusgh096@naver.com>
2021-11-14 00:19:47 +0900

    커뮤니티 디테일 cyh

--- app/src/main/java/com/fourth/ondaeng/CommunityDetailActivity.java
+++ app/src/main/java/com/fourth/ondaeng/CommunityDetailActivity.java
@@ -8,8 +8,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.fourth.ondaeng.databinding.ActivityCommunityBinding;
+import com.fourth.ondaeng.databinding.ActivityCommunityDetailBinding;
+import com.fourth.ondaeng.databinding.ActivityDrawerBinding;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,136 +37,90 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class CommunityDetailActivity extends AppCompatActivity {
 
-    ActivityCommunityBinding binding;
+    ActivityCommunityDetailBinding binding;
 
-    TextView tv_title;
-    TextView tv_content;
-    Button b_back;
 
-    String board_seq = ""; //선택한 게시물의 번호
-    String userid = "";
-
+//    int getNo = getIntent().getIntExtra("postNo",1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityCommunityBinding.inflate(getLayoutInflater());
+        binding = ActivityCommunityDetailBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        int getNo = getIntent().getIntExtra("postNo",1);
 
-        //CommunityActivity에서 넘긴 변수
-        board_seq = getIntent().getStringExtra("board_seq");
-        userid = getIntent().getStringExtra("userid");
+        readPost(getNo);
 
-        //컴포넌트 초기화
-        tv_title = findViewById(R.id.tv_title);
-        tv_content = findViewById(R.id.tv_content);
-        b_back = findViewById(R.id.b_back);
 
-        //목록으로 버튼 누르면 뒤로가기
-        b_back.setOnClickListener(new View.OnClickListener() {
+
+        binding.bBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), CommunityActivity.class);
-                intent.putExtra("userid", userid);
-                startActivity(intent);
+                onBackPressed();
             }
         });
-
-
-        //해당 게시물 데이터 불러오기
-        //InitData();
     }
 
-    /*
-    private void InitData() {
+//    게시물 읽어오기
+    public void readPost(int postNo){
+//        easyToast("getPwById 실행됨");
+        String url = "http://14.55.65.181/ondaeng/readPost?";
+        //JSON형식으로 데이터 통신을 진행합니다!
+        JSONObject testjson = new JSONObject();
+        try {
+            //입력해둔 edittext의 id와 pw값을 받아와 put해줍니다 : 데이터를 json형식으로 바꿔 넣어주었습니다.
 
-        LoadBoard loadBoard = new LoadBoard();
-        loadBoard.execute(board_seq);
+            url = url +"postNo="+postNo;
 
-    }
+            //이제 전송
+            final RequestQueue requestQueue = Volley.newRequestQueue(CommunityDetailActivity.this);
 
-    class LoadBoard extends AsyncTask<String, void, String> {
+//            easyToast(url);
+            final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url,null, new Response.Listener<JSONObject>() {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
+                //데이터 전달을 끝내고 이제 그 응답을 받을 차례입니다.
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+//                        easyToast("응답");
+                        //받은 json형식의 응답을 받아
+                        //key값에 따라 value값을 쪼개 받아옵니다.
+                        JSONObject jsonObject = new JSONObject(response.toString());
+                        easyToast(Integer.valueOf(jsonObject.getJSONArray("data").length()));
+                        JSONObject data = new JSONObject(jsonObject.getJSONArray("data").get(0).toString());
+                        String title =data.get("title").toString();
+                        String content =data.get("content").toString();
+                        binding.tvTitle.setText(title);
+                        binding.tvContent.setText(content);
 
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            try {
-                // 결과값이 JSONArray 형태로 넘어오기 때문에
-                // JSONArray, JSONObject 를 사용해서 파싱
-                JSONArray jsonArray = null;
-                jsonArray = new JSONArray(result);
-
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-
-                    // Database 의 데이터들을 변수로 저장한 후 해당 TextView 에 데이터 입력
-                    String title = jsonObject.optString("title");
-                    String content = jsonObject.optString("content");
-
-                    tv_title.setText(title);
-                    tv_content.setText(content);
-                }
-
-                } catch(JSONException e){
-                    e.printStackTrace();
-                }
-            }
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            String board_seq = params[0];
-            String server_url = "http://14.55.65.181/ondaeng/getMemberId?";
-
-
-            URL url;
-            String response = "";
-            try {
-                url = new URL(server_url);
-
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(15000);
-                conn.setConnectTimeout(15000);
-                conn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-                Uri.Builder builder = new Uri.Builder()
-                        .appendQueryParameter("board_seq", board_seq);
-                String query = builder.build().getEncodedQuery();
-
-                OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-                writer.write(query);
-                writer.flush();
-                writer.close();
-                os.close();
-
-                conn.connect();
-                int responseCode=conn.getResponseCode();
-
-                if (responseCode == HttpsURLConnection.HTTP_OK) {
-                    String line;
-                    BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    while ((line=br.readLine()) != null) {
-                        response+=line;
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }
-                else {
-                    response="";
-
+                //서버로 데이터 전달 및 응답 받기에 실패한 경우 아래 코드가 실행됩니다.
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            });
+            jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            requestQueue.add(jsonObjectRequest);
 
-            return response;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-     */
+    }
 
+
+    //뒤로가기 키 눌렀을 때
+    @Override
+    public void onBackPressed() {
+        CommunityDetailActivity.this.startActivity(new Intent(CommunityDetailActivity.this, CommunityActivity.class));
+        CommunityDetailActivity.this.finish();
+    }
+
+    void easyToast(Object obj){
+        Toast.makeText(getApplicationContext(),obj.toString(),Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/com/fourth/ondaeng/postData.java
+++ app/src/main/java/com/fourth/ondaeng/postData.java
@@ -4,11 +4,14 @@ public class postData {
     private String id;
     private String title;
     private String date;
+    private int postNo;
 
-    public postData(String id, String title, String date){
+    public postData(String id, String title, String date, int postNo){
         this.id = id;
         this.title = title;
         this.date = date;
+        this.postNo = postNo;
+
     }
 
     public String getid()
@@ -24,6 +27,11 @@ public class postData {
     public String getdate()
     {
         return this.date;
+    }
+
+    public int getpostNo()
+    {
+        return this.postNo;
     }
 
 }
